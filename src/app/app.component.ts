385635c3e6f10dbcd78075e10e4510108f90e304
Cristobal Muñoz <103208607+CristMun@users.noreply.github.com>
2022-11-09 14:21:24 -0300

    Se agregan botones Clima y Mapa en sidemenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,12 @@ export class AppComponent {
   public appPages = [
     { title: 'Home', url: '/home', icon: 'home' },
     { title: 'Asistencia', url: '/asistencia', icon: 'calendar' },
+    { title: 'Clima', url: '/mapa', icon: 'sunny' },
+    { title: 'Mapa', url: '/mapa', icon: 'map' },
     { title: 'Coversor', url: '/coversor', icon: 'hammer' },
     { title: 'About', url: '/about', icon: 'alert' },
+    
+
     
 
   ];
